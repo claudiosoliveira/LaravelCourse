464bea05dc7184ee00504d3979d2c60fab42be3e
Claudio Santos Oliveira <monfredvoncastein@gmail.com>
2021-03-25 06:59:28 -0300

    SoftDelete Implemented

--- database/migrations/2021_03_10_233520_create_brands.php
+++ database/migrations/2021_03_10_233520_create_brands.php
@@ -16,6 +16,7 @@ class CreateBrands extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
